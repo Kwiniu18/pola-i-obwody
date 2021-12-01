3af1f7f9a1bb8f15fa2af453b6788b91c85a2fa0
Kwnq <44814268+Kwiniu18@users.noreply.github.com>
2021-12-01 14:09:35 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,11 +5,11 @@ from figures import Circle
 
 def test_is_triangle_possible():
     result = Triangle(1, 2, 3, 4)
-    assert result.is_triangle(1, 2, 3)
+    assert result.is_triangle(3, 4, 5)
 
 
 def test_is_rectangle_possible():
-    result = Rectangle(3, 3)
+    result = Rectangle(1, 2)
     assert result.is_rectangle_possible(3, 3)
 
 
@@ -17,3 +17,41 @@ def test_is_circle_possible():
     result = Circle(1)
     assert result.is_circle_possible(1)
 
+
+# RECTANGLE TESITNG
+def test_rectangle_area_calculations():
+    result = Rectangle(4, 4)
+    out = result.area()
+    assert out == 16
+
+
+def test_rectangle_perimeter_calculations():
+    result = Rectangle(4, 4)
+    out = result.perimeter()
+    assert out == 16
+
+
+# TRIANGLE TESTING
+def test_triangle_perimeter_calculations():
+    result = Triangle(5, 5, 5, 0)
+    out = result.perimeter()
+    assert out == 15
+
+
+def test_triangle_area_calculations():
+    result = Triangle(6, 0, 0, 2)
+    out = result.area()
+    assert out == 6
+
+
+##CIRCLE TESTING
+def test_circle_area_calculations():
+    result = Circle(1)
+    out = result.area(2)
+    assert out == 12.566370614359172
+
+
+def test_circle_perimeter_calculations():
+    result = Circle(1)
+    out = result.perimeter(2)
+    assert out == 12.566370614359172
